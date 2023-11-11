79a089f7c90e2fc9b5cfc3d4dba9f0aff469e32e
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-11 19:18:30 +0300

    feat: Homepage started

---
+++ src/router.jsx
@@ -0,0 +1,8 @@
+import Home from "./pages/Home";
+import { createBrowserRouter } from "react-router-dom";
+export const router = createBrowserRouter([
+  {
+    element: <Home />,
+    index: true,
+  },
+]);
