f5e062aff075cbb70f3b56cdb3bc9d25f0e371eb
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-21 23:30:17 +0300

    fix: user req

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { getUserByIdReq } from "../api";
 export default function Profile() {
   const id = useParams().id;
   console.log(id);
-  const user = getUserByIdReq(id).then((res) => res);
+  const user = getUserByIdReq(id);
   console.log(user);
   return (
     <Stack direction="column" gap={2} placeItems="center" className="grayBlock">
