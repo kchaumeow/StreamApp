a0bfc00f5b97e12ce2f388fa209cda5890fbb683
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-21 22:02:43 +0300

    fix: user req

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,14 +4,12 @@ import { useParams } from "react-router-dom";
 export default function Profile() {
   const id = useParams();
   let user = null;
-  useEffect(() => {
-    try {
-      user = getUserByIdReq(id).then((res) => res);
-      console.log(user);
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+  try {
+    user = getUserByIdReq(id).then((res) => res);
+    console.log(user);
+  } catch (err) {
+    console.log(err);
+  }
   return (
     <Stack direction="column" gap={2} placeItems="center" className="grayBlock">
       <Heading className="grayBlock">{user.name}</Heading>
