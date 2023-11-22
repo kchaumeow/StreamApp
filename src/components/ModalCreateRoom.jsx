95b61508ca7c932d7030505c13f9badeed826b53
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-22 22:47:50 +0300

    fix: ui

--- src/components/ModalCreateRoom.jsx
+++ src/components/ModalCreateRoom.jsx
@@ -112,7 +112,7 @@ export default function ModalCreateRoom() {
                 onClose();
               }}
               variant="outline"
-              bgColor="red"
+              colorScheme="red"
               mr={3}
             >
               Close
