d0ad52a7a5869352b8c72ca35d1d29d92f8e1dd7
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-23 11:34:35 +0300

    fix: no user no modal

--- src/components/RoomsStack.jsx
+++ src/components/RoomsStack.jsx
@@ -1,8 +1,11 @@
 import { Stack } from "@chakra-ui/react";
 import RoomCard from "./RoomCard";
 import ModalCreateRoom from "./ModalCreateRoom";
+import { useSelector } from "react-redux";
+import { userSelector } from "../store/selectors";
 
 export default function RoomsStack({ rooms }) {
+  const user = useSelector(userSelector);
   return (
     <Stack className="grayBlock" placeItems="center" gap={3}>
       <Stack
@@ -16,7 +19,7 @@ export default function RoomsStack({ rooms }) {
           return <RoomCard key={room.id} id={room.name} desc={room.desc} />;
         })}
       </Stack>
-      <ModalCreateRoom />
+      {user && <ModalCreateRoom />}
     </Stack>
   );
 }
