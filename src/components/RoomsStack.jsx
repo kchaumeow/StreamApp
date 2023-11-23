6f3fe89f4e6bae56a9165dadafa58f5ab4270156
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-23 11:36:45 +0300

    fix: ui

--- src/components/RoomsStack.jsx
+++ src/components/RoomsStack.jsx
@@ -7,7 +7,7 @@ import { userSelector } from "../store/selectors";
 export default function RoomsStack({ rooms }) {
   const user = useSelector(userSelector);
   return (
-    <Stack className="grayBlock" placeItems="center" gap={3}>
+    <Stack className="grayBlock" placeItems="center" gap={15}>
       <Stack
         direction="row"
         flexWrap="wrap"
