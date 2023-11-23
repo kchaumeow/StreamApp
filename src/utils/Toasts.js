965e15e8f0989e45fcfe82e23e7e53c3637963f8
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-23 09:08:46 +0300

    fix; toast minimise

--- src/utils/Toasts.js
+++ src/utils/Toasts.js
@@ -1,19 +1,21 @@
+const basicToast = {
+  duration: 3000,
+  isClosable: true,
+  position: "bottom-left",
+};
+
 export const showErrorLogOpts = {
   title: "Account wasn't found.",
   description: "Check for errors in your info",
   status: "error",
-  duration: 3000,
-  isClosable: true,
-  position: "bottom-left",
+  basicToast,
 };
 
 export const showSuccessLogOpts = {
   title: "Success",
   description: "You are logged in",
   status: "success",
-  duration: 3000,
-  isClosable: true,
-  position: "bottom-left",
+  basicToast,
 };
 
 export function showErrorRegOpts(desc) {
@@ -21,9 +23,7 @@ export function showErrorRegOpts(desc) {
     title: "Error creating account",
     description: desc,
     status: "error",
-    duration: 3000,
-    isClosable: true,
-    position: "bottom-left",
+    basicToast,
   };
 }
 
@@ -31,16 +31,12 @@ export const showErrorRoomName = {
   title: "Error creating room",
   description: "Room with this name already exists",
   status: "error",
-  duration: 3000,
-  isClosable: true,
-  position: "bottom-left",
+  basicToast,
 };
 
 export const showSuccessRoomCreate = {
   title: "Success",
   description: "New room was created!",
   status: "success",
-  duration: 3000,
-  isClosable: true,
-  position: "bottom-left",
+  basicToast,
 };
