0cfe1d4eb5c821b95134f8f45149f49f49ef0c23
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-23 14:58:56 +0300

    feat: no user home page

--- src/components/RoomsStack.jsx
+++ src/components/RoomsStack.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@chakra-ui/react";
+import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 import RoomCard from "./RoomCard";
 import ModalCreateRoom from "./ModalCreateRoom";
 import { useSelector } from "react-redux";
@@ -9,7 +9,7 @@ export default function RoomsStack() {
   const rooms = useSelector(roomsSelector);
   return (
     <Stack className="grayBlock" placeItems="center" spacing={15}>
-      {user && (
+      {user ? (
         <>
           <Stack
             direction="row"
@@ -24,6 +24,21 @@ export default function RoomsStack() {
           </Stack>
           <ModalCreateRoom />
         </>
+      ) : (
+        <Box textAlign="center" py={10} px={6} className="grayBlock">
+          <Heading
+            display="inline-block"
+            as="h2"
+            size="2xl"
+            bgGradient="linear(to-r, green.400, green.600)"
+            backgroundClip="text"
+          >
+            Unauthorised
+          </Heading>
+          <Text fontSize="18px" mt={3} mb={2}>
+            Login or Register to continue
+          </Text>
+        </Box>
       )}
     </Stack>
   );
