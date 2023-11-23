d5dc8a5eff2f5a0c6118b7d1e0d9ad409cb8d9d6
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-23 12:36:13 +0300

    fix: rooms

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import RoomsStack from "../components/RoomsStack";
 import { useDispatch, useSelector } from "react-redux";
-import { roomsSelector, userSelector } from "../store/selectors";
+import { userSelector } from "../store/selectors";
 import { setRooms } from "../store/roomSlice";
 
 export default function Home() {
   const dispatch = useDispatch();
   const user = useSelector(userSelector);
-  const roomsRedux = useSelector(roomsSelector);
   useEffect(() => {
     if (user) {
       dispatch(setRooms(user.id));
